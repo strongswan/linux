5c4872dfab5f28dbea469c705a0919cd2435478c
Martin Willi <martin@strongswan.org>
2011-05-19 10:17:59 +0200

    netfilter: Add hooks before XFRM input and output processing
    To allow netfilter hooks to modify, inspect or drop packets before
    XFRM processing, two hooks are added to the netfilter framework.
    The first is invoked just before XFRM input processing, the other
    before XFRM output processing. This allows a hook to drop the packet,
    but mangle the associated xfrm state (sequence numbers, for example).
    
    NF_INET_NUMHOOKS does not include the new hooks. This does not allow the use
    of the new hooks from userspace, but keeps the ABI as it is.
    
    Signed-off-by: Martin Willi <martin@strongswan.org>

--- net/xfrm/xfrm_input.c
+++ net/xfrm/xfrm_input.c
@@ -9,6 +9,7 @@
 
 #include <linux/slab.h>
 #include <linux/module.h>
+#include <linux/netfilter.h>
 #include <linux/netdevice.h>
 #include <net/dst.h>
 #include <net/ip.h>
@@ -101,6 +102,14 @@ int xfrm_prepare_input(struct xfrm_state *x, struct sk_buff *skb)
 	return inner_mode->input2(x, skb);
 }
 EXPORT_SYMBOL(xfrm_prepare_input);
+
+static int xfrm_type_input(struct sk_buff *skb)
+{
+	struct xfrm_state *x;
+
+	x = skb->sp->xvec[skb->sp->len - 1];
+	return x->type->input(x, skb);
+}
 
 int xfrm_input(struct sk_buff *skb, int nexthdr, __be32 spi, int encap_type)
 {
@@ -197,9 +206,10 @@ int xfrm_input(struct sk_buff *skb, int nexthdr, __be32 spi, int encap_type)
 
 		skb_dst_force(skb);
 
-		nexthdr = x->type->input(x, skb);
+		nexthdr = NF_HOOK(family, NF_INET_XFRM_IN, skb,
+				  skb->dev, NULL, xfrm_type_input);
 
-		if (nexthdr == -EINPROGRESS)
+		if (nexthdr == -EINPROGRESS || nexthdr == -EPERM)
 			return 0;
 
 resume:
