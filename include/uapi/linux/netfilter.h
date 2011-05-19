a4a20ff3219fd9e0f4fb5d93a42476b51b308449
Martin Willi <martin@strongswan.org>
2011-05-19 10:17:59 +0200

    netfilter: Add hooks before XFRM input and output processing
    To allow netfilter hooks to modify, inspect or drop packets before
    XFRM processing, two hooks are added to the netfilter framework.
    The first is invoked just before XFRM input processing, the other
    before XFRM output processing. This allows a hook to drop the packet,
    but mangle the associated xfrm state (sequence numbers, for example).
    
    NF_INET_NUMHOOKS does not include the new hooks. This does not allow the use
    of the new hooks from userspace, but keeps the ABI as it is.
    
    Signed-off-by: Martin Willi <martin@strongswan.org>

--- include/uapi/linux/netfilter.h
+++ include/uapi/linux/netfilter.h
@@ -47,6 +47,14 @@ enum nf_inet_hooks {
 	NF_INET_POST_ROUTING,
 	NF_INET_NUMHOOKS,
 	NF_INET_INGRESS = NF_INET_NUMHOOKS,
+
+#ifdef __KERNEL__
+	/* not accessible from userspace, keep iptables ABI. */
+	NF_INET_XFRM_IN = NF_INET_NUMHOOKS,
+	NF_INET_XFRM_OUT,
+
+	NF_MAX_HOOKS
+#endif
 };
 
 enum nf_dev_hooks {
