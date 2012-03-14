fe89381d23608f95a8b11c201711886b278fc092
Martin Willi <martin@strongswan.org>
2012-03-14 12:20:42 +0100

    xfrm: Add XFRM replay failover function to increment outgoing sequence numbers
    If a node takes over an IPsec SA from another, it is critical that the new
    node does not use a lower sequence number for outgoing packets. To avoid this
    situation, add a function to XFRM replay implementations to jump to a higher
    sequence number.
    
    Signed-off-by: Martin Willi <martin@strongswan.org>

--- net/xfrm/xfrm_replay.c
+++ net/xfrm/xfrm_replay.c
@@ -104,6 +104,23 @@ static int xfrm_replay_overflow(struct xfrm_state *x, struct sk_buff *skb)
 	return err;
 }
 
+static void xfrm_replay_failover(struct xfrm_state *x, int ratio)
+{
+	struct net *net = xs_net(x);
+	u32 incr;
+
+	if (x->type->flags & XFRM_TYPE_REPLAY_PROT) {
+
+		incr = x->replay.oseq / ratio;
+		if (U32_MAX - x->replay.oseq > incr) {
+			x->replay.oseq += incr;
+
+			if (xfrm_aevent_is_on(net))
+				x->repl->notify(x, XFRM_REPLAY_UPDATE);
+		}
+	}
+}
+
 static int xfrm_replay_check(struct xfrm_state *x,
 		      struct sk_buff *skb, __be32 net_seq)
 {
@@ -182,6 +199,24 @@ static int xfrm_replay_overflow_bmp(struct xfrm_state *x, struct sk_buff *skb)
 	}
 
 	return err;
+}
+
+static void xfrm_replay_failover_bmp(struct xfrm_state *x, int ratio)
+{
+	struct xfrm_replay_state_esn *replay_esn = x->replay_esn;
+	struct net *net = xs_net(x);
+	u32 incr;
+
+	if (x->type->flags & XFRM_TYPE_REPLAY_PROT) {
+
+		incr = replay_esn->oseq / ratio;
+		if (U32_MAX - replay_esn->oseq > incr) {
+			replay_esn->oseq += incr;
+
+			if (xfrm_aevent_is_on(net))
+				x->repl->notify(x, XFRM_REPLAY_UPDATE);
+		}
+	}
 }
 
 static int xfrm_replay_check_bmp(struct xfrm_state *x,
@@ -420,6 +455,31 @@ static int xfrm_replay_overflow_esn(struct xfrm_state *x, struct sk_buff *skb)
 	}
 
 	return err;
+}
+
+static void xfrm_replay_failover_esn(struct xfrm_state *x, int ratio)
+{
+	struct xfrm_replay_state_esn *replay_esn = x->replay_esn;
+	struct net *net = xs_net(x);
+	u32 incr;
+
+	if (x->type->flags & XFRM_TYPE_REPLAY_PROT) {
+
+		if (replay_esn->oseq_hi)
+			incr = U32_MAX / ratio;
+		else
+			incr = replay_esn->oseq / ratio;
+		if (U32_MAX - replay_esn->oseq < incr) {
+			replay_esn->oseq_hi++;
+			if (replay_esn->oseq_hi == 0) {
+				replay_esn->oseq_hi--;
+				incr = 0;
+			}
+		}
+		replay_esn->oseq += incr;
+		if (xfrm_aevent_is_on(net))
+			x->repl->notify(x, XFRM_REPLAY_UPDATE);
+	}
 }
 
 static int xfrm_replay_check_esn(struct xfrm_state *x,
@@ -686,6 +746,7 @@ static const struct xfrm_replay xfrm_replay_legacy = {
 	.recheck	= xfrm_replay_check,
 	.notify		= xfrm_replay_notify,
 	.overflow	= xfrm_replay_overflow_offload,
+	.failover	= xfrm_replay_failover,
 };
 
 static const struct xfrm_replay xfrm_replay_bmp = {
@@ -694,6 +755,7 @@ static const struct xfrm_replay xfrm_replay_bmp = {
 	.recheck	= xfrm_replay_check_bmp,
 	.notify		= xfrm_replay_notify_bmp,
 	.overflow	= xfrm_replay_overflow_offload_bmp,
+	.failover	= xfrm_replay_failover_bmp,
 };
 
 static const struct xfrm_replay xfrm_replay_esn = {
@@ -702,6 +764,7 @@ static const struct xfrm_replay xfrm_replay_esn = {
 	.recheck	= xfrm_replay_recheck_esn,
 	.notify		= xfrm_replay_notify_esn,
 	.overflow	= xfrm_replay_overflow_offload_esn,
+	.failover	= xfrm_replay_failover_esn,
 };
 #else
 static const struct xfrm_replay xfrm_replay_legacy = {
@@ -710,6 +773,7 @@ static const struct xfrm_replay xfrm_replay_legacy = {
 	.recheck	= xfrm_replay_check,
 	.notify		= xfrm_replay_notify,
 	.overflow	= xfrm_replay_overflow,
+	.failover	= xfrm_replay_failover,
 };
 
 static const struct xfrm_replay xfrm_replay_bmp = {
@@ -718,6 +782,7 @@ static const struct xfrm_replay xfrm_replay_bmp = {
 	.recheck	= xfrm_replay_check_bmp,
 	.notify		= xfrm_replay_notify_bmp,
 	.overflow	= xfrm_replay_overflow_bmp,
+	.failover	= xfrm_replay_failover_bmp,
 };
 
 static const struct xfrm_replay xfrm_replay_esn = {
@@ -726,6 +791,7 @@ static const struct xfrm_replay xfrm_replay_esn = {
 	.recheck	= xfrm_replay_recheck_esn,
 	.notify		= xfrm_replay_notify_esn,
 	.overflow	= xfrm_replay_overflow_esn,
+	.failover	= xfrm_replay_failover_esn,
 };
 #endif
 
